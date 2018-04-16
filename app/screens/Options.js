0d5fcfd81d73e23a7f60479483f4ce23f3d7de89
Yan Pengqiang <yan2010@live.com>
2018-04-16 22:32:04 +0800

    complete module-4-lesson-5-ExternalLinks

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -3,6 +3,7 @@ import {
   ScrollView,
   StatusBar,
   Platform,
+  Linking,
 } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { PropTypes } from 'prop-types'
@@ -23,7 +24,7 @@ class Options extends Component {
   }
 
   handleSitePress = () => {
-    console.log('press site')
+    Linking.openURL('https://fixer.io').catch(() => alert('An error occured!'))
   }
 
   render() {
